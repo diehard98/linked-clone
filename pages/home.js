9ade2d6aeefccc18c94e89adfc1d5f1e0118c67c
YONGWOOK SONG <diehard98@gmail.com>
2022-02-14 14:57:56 -0500

    landing page is done

--- pages/home.js
+++ pages/home.js
@@ -8,7 +8,7 @@ import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRound
 
 function Home() {
   return (
-    <div>
+    <div className="space-y-10 relative">
       <header className="flex justify-around items-center py-4">
         <div className="relative w-36 h-10">
           <Image
@@ -31,6 +31,32 @@ function Home() {
           </div>
         </div>
       </header>
+
+      <main className="flex flex-col xl:flex-row items-center max-w-screen-lg mx-auto">
+        <div className="space-y-6 xl:space-y-10">
+          <h1 className="text-3xl md:text-5xl text-amber-800/80 max-w-xl !leading-snug pl-4 xl:pl-0">
+            Welcome to your professional comminity
+          </h1>
+          <div className="space-y-4">
+            <div className="intent">
+              <h2 className="text-xl">Search for a job</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
+            </div>
+            <div className="intent">
+              <h2 className="text-xl">Find a person you know</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
+            </div>
+            <div className="intent">
+              <h2 className="text-xl">Learn a new skill</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
+            </div>
+          </div>
+        </div>
+
+        <div className="relative xl:absolute w-80 h-80 xl:w-[650px] xl:h-[650px] top-14 right-5">
+          <Image src="https://rb.gy/vkzpzt" layout="fill" priority />
+        </div>
+      </main>
     </div>
   );
 }
