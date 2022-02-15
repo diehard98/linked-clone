9851485f5bd7e23e86eb4b705d1038be332ac9a8
YONGWOOK SONG <diehard98@gmail.com>
2022-02-15 09:47:07 -0500

    Main header component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { signOut } from "next-auth/react";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -9,8 +10,8 @@ export default function Home() {
       <Head>
         <title>LinkedIn</title>
       </Head>
-
-      <button onClick={signOut}>Sign Out</button>
+      <Header />
+      {/* <button onClick={signOut}>Sign Out</button> */}
     </div>
   );
 }
